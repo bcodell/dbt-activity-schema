137b51c86fc4235bc7a0d84a3c593445c03b7e89
Bryce Codell <bryce.codell@24analytics.co>
2023-05-19 22:02:14 -0400

    add tests for dataset column functionality

--- integration_tests/models/datasets/primary__all/dataset__select_all_aggregate_after.sql
+++ integration_tests/models/datasets/primary__all/dataset__select_all_aggregate_after.sql
@@ -9,9 +9,11 @@ aggregate after visited_page (
     count(activity_id) as pages_visited_after
 )
 aggregate after bought_something(
-    sum(total_items_purchased) as total_items_purchased_after,
     sum(total_sales) as total_sales_after,
     count(activity_id) as total_purchases_after
+)
+include (
+    total_items_purchased_after
 )
 {% endset %}
 
