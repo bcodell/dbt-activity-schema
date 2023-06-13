5895ec4fb2dc2e292ceba78258e7ced0d0b8ce6c
Bryce Codell <bryce.codell@24analytics.co>
2023-06-12 22:33:30 -0400

    add model and column name context checks

--- macros/activity_schema/activity/column.sql
+++ macros/activity_schema/activity/column.sql
@@ -9,8 +9,30 @@
 {%- set project_name = model['unique_id'].split(".")[1] -%}
 {%- if execute -%}
     {%- set model_prefix = var("dbt_aql").get("streams").get(stream, {}).get("model_prefix", "") -%}
-    {%- set activity_node_config = graph.get("nodes", {}).get("model."~project_name~"."~model_prefix~activity_name, {}).get("config", {}) -%}
-    {%- set data_type = activity_node_config.get("data_types", {}).get(column_name, dbt.type_string()) -%}
+    {%- set activity_node = graph.get("nodes", {}).get("model."~project_name~"."~model_prefix~activity_name, none) -%}
+
+    {%- if activity_node is none -%}
+        {%- set error_message -%}
+aql query in model '{{ model.unique_id }}' has invalid syntax. Please choose a valid activity - selected '{{activity_name}}'.
+Be sure to check aql in dataset column models.
+        {%- endset -%}
+        {{ exceptions.raise_compiler_error(error_message) }}
+    {% endif %}
+
+    {%- set activity_node_config = activity_node.get("config", {}) -%}
+    {%- set data_types = activity_node_config.get("data_types", {}) -%}
+    {%- set customer_column = dbt_aql.customer_column(stream) -%}
+    {%- do data_types.update({customer_column: type_string()}) -%}
+    {%- do data_types.update(dbt_aql.schema_column_types()) -%}
+    {%- if column_name not in data_types.keys() -%}
+        {%- set error_message -%}
+aql query in model '{{ model.unique_id }}' has invalid syntax. Column '{{column_name}}' is not registered with a data type
+in activity '{{activity_name}}'. Please update the model config appropriately for the activity.
+Be sure to check aql in dataset column models.
+        {%- endset -%}
+        {{ exceptions.raise_compiler_error(error_message) }}
+    {%- endif -%}
+    {%- set data_type = data_types.get(column_name, dbt.type_string()) -%}
 {%- else -%}
 {%- set data_type = dbt.type_string() -%}
 {%- endif -%}
--- macros/activity_schema/dataset/dataset_column/_build_activity_from_dataset_column.sql
+++ macros/activity_schema/dataset/dataset_column/_build_activity_from_dataset_column.sql
@@ -9,36 +9,46 @@
         {%- set dataset_column = model_prefix~dataset_column -%}
     {%- endif -%}
 
-    {%- set activity_node = graph.get("nodes", {}).get("model."~project_name~"."~dataset_column, {}) -%}
+    {%- set activity_node = graph.get("nodes", {}).get("model."~project_name~"."~dataset_column, none) -%}
+    {%- if activity_node is none -%}
+        {%- set error_message -%}
+aql query in model '{{ model.unique_id }}' has invalid syntax. Please choose a valid dataset column - selected '{{dataset_column}}'
+Full dataset column query:
+
+{{aql}}
+        {%- endset -%}
+        {{ exceptions.raise_compiler_error(error_message) }}
+    {% endif %}
+
     {%- set aql = activity_node.get("config", {}).get("aql", none) -%}
     {%- if aql is none -%}
         {%- set error_message -%}
     Dataset column model '{{dataset_column}}' does not have a required config key called `aql`.
         {%- endset -%}
         {{ exceptions.raise_compiler_error(error_message) }}
-    {%- else -%}
-
-        {%- set query_no_comments = dbt_aql._strip_comments(aql) -%}
-        {%- set query_clean = dbt_aql._clean_query(query_no_comments) -%}
-        {%- set using, rest = dbt_aql._parse_keyword(query_clean, ["using"]) -%}
-        {%- set stream, rest = dbt_aql._parse_stream(rest) -%}
-        {%- set activity, rest = dbt_aql._parse_activity(rest, stream, [av.aggregate, av.append]) -%}
-
-        {%- if activity.columns|length != 1 -%}
-            {%- set error_message -%}
-        Dataset column model '{{dataset_column}}' should only specify one column but it appears to specify multiple.
-        Full query:
-
-        {{aql}}
-            {%- endset -%}
-            {{ exceptions.raise_compiler_error(error_message) }}
-        {%- else -%}
-            {%- set col = activity.columns[0] -%}
-            {%- set col.alias = dataset_column.replace(model_prefix, "") -%}
-            {%- set activity.columns = [col] -%}
-        {%- endif -%}
-        {%- do return(activity) -%}
     {%- endif -%}
+
+    {%- set query_no_comments = dbt_aql._strip_comments(aql) -%}
+    {%- set query_clean = dbt_aql._clean_query(query_no_comments) -%}
+    {%- set using, rest = dbt_aql._parse_keyword(query_clean, ["using"]) -%}
+    {%- set stream, rest = dbt_aql._parse_stream(rest) -%}
+    {%- set activity, rest = dbt_aql._parse_activity(rest, stream, [av.aggregate, av.append]) -%}
+
+    {%- if activity.columns|length != 1 -%}
+        {%- set error_message -%}
+Dataset column model '{{dataset_column}}' should only specify one column but it appears to specify multiple.
+Full dataset column query:
+
+{{aql}}
+        {%- endset -%}
+        {{ exceptions.raise_compiler_error(error_message) }}
+    {%- endif -%}
+
+    {%- set col = activity.columns[0] -%}
+    {%- set col.alias = dataset_column.replace(model_prefix, "") -%}
+    {%- set activity.columns = [col] -%}
+
+    {%- do return(activity) -%}
 {%- else -%}
     {%- do return(none) -%}        
 {%- endif -%}
