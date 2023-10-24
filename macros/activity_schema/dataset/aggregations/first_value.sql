5b6681d78f7b5eb560316ea80150ea83338f3519
Tony Tushar <tony@revunit.com>
2023-10-24 16:21:27 -0500

    use dbt cross db split part for better compatibility

--- macros/activity_schema/dataset/aggregations/first_value.sql
+++ macros/activity_schema/dataset/aggregations/first_value.sql
@@ -6,15 +6,13 @@
 {%- set joined = dbt_aql.joined() -%}
 {%- set ts = dbt_aql.schema_columns().ts -%}
 {%- set delimiter = ";.,;" -%}
-cast(split_part(
-            min(
-                cast({{joined}}.{{ts}} as {{dbt.type_string()}})
-                || {{dbt.string_literal(delimiter)}}
-                || cast({{ column.column_sql }} as {{dbt.type_string()}})
-            ),
-            {{dbt.string_literal(delimiter)}},
-            2
-        ) as {{column.data_type}})
+cast(
+    {{ dbt.split_part(
+        string_text='cast(' ~ joined ~ '.' ~ ts ~ ' as ' ~ dbt.type_string() ~ ')' || dbt.string_literal(delimiter) || 'cast(' ~ column.column_sql ~ ' as ' ~ dbt.type_string() ~ ')',
+        delimiter_text=dbt.string_literal(delimiter),
+        part_number=2
+    ) }}
+as {{column.data_type}})
 {%- endmacro -%}
 
 {% macro duckdb__aggfunc_first_value(column) %}
