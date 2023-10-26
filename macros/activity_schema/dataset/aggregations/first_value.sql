69f683404445d769fd0a85521b2afd23acda25d8
Bryce Codell <bryce@ashbyhq.com>
2023-10-26 16:02:54 -0400

    add bq implementation for first and last value aggfuncs

--- macros/activity_schema/dataset/aggregations/first_value.sql
+++ macros/activity_schema/dataset/aggregations/first_value.sql
@@ -36,3 +36,17 @@ cast(string_split(
 {%- set ts = dbt_aql.schema_columns().ts -%}
 min_by({{column.column_sql}}, {{joined}}.{{ts}})
 {% endmacro %}
+
+{% macro bigquery__aggfunc_first_value(column) %}
+{%- set joined = dbt_aql.joined() -%}
+{%- set ts = dbt_aql.schema_columns().ts -%}
+{%- set delimiter = ";.,;" -%}
+cast(split(
+            min(
+                cast({{joined}}.{{ts}} as {{dbt.type_string()}})
+                || {{dbt.string_literal(delimiter)}}
+                || cast({{ column.column_sql }} as {{dbt.type_string()}})
+            ),
+            {{dbt.string_literal(delimiter)}}
+        )[safe_offset(1)] as {{column.data_type}})
+{%- endmacro -%}
--- macros/activity_schema/dataset/aggregations/last_value.sql
+++ macros/activity_schema/dataset/aggregations/last_value.sql
@@ -36,3 +36,17 @@ cast(string_split(
 {%- set ts = dbt_aql.schema_columns().ts -%}
 max_by({{column.column_sql}}, {{joined}}.{{ts}})
 {% endmacro %}
+
+{% macro bigquery__aggfunc_last_value(column) %}
+{%- set joined = dbt_aql.joined() -%}
+{%- set ts = dbt_aql.schema_columns().ts -%}
+{%- set delimiter = ";.,;" -%}
+cast(split(
+            max(
+                cast({{joined}}.{{ts}} as {{dbt.type_string()}})
+                || {{dbt.string_literal(delimiter)}}
+                || cast({{ column.column_sql }} as {{dbt.type_string()}})
+            ),
+            {{dbt.string_literal(delimiter)}}
+        )[safe_offset(1)] as {{column.data_type}})
+{%- endmacro -%}
