94f7558e1b5dc803cb97929c7e8b85d4f4df37bf
Bryce Codell <bryce@ashbyhq.com>
2023-12-14 22:05:50 -0500

    add cluster keys to activity models

--- integration_tests/models/activities/customer__bought_something.sql
+++ integration_tests/models/activities/customer__bought_something.sql
@@ -1,4 +1,19 @@
+{% set keys = dbt_aql.cluster_keys(stream='customer_stream') %}
+{% if target.name == 'bigquery' %}
+    {% set cluster_keys = keys.cluster_by %}
+    {% set partition_keys = keys.partition_by %}
+{# TODO: remove snowflake-specific implementation once localstack bugs are fixed #}
+{% elif target.name == 'snowflake' %}
+    {% set cluster_keys = none %}
+    {% set partition_keys = none %}
+{% else %}
+    {% set cluster_keys = keys %}
+    {% set partition_keys = '' %}
+{% endif %}
+
 {{ config(
+    cluster_by=cluster_keys,
+    partition_by=partition_keys,
     data_types={
         'total_sales': dbt.type_int(),
         'total_items_purchased': dbt.type_int(),
