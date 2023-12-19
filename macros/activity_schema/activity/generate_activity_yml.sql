ccba5a6bcdbfa3edf8e160e8af6435ddcd8a1028
Andreas Wöhrl <a.woehrl@n-pg.de>
2023-12-19 12:03:15 +0100

    Use dynamic column labels

--- macros/activity_schema/activity/generate_activity_yml.sql
+++ macros/activity_schema/activity/generate_activity_yml.sql
@@ -10,29 +10,48 @@
 
 -- Get column descriptions and tests
 {% macro generate_columns(activity) %}
-  {% set columns = [
-      {'name': 'activity_id', 'description': 'Unique identifier for the activity.', 'data_type': type_string(), 'tests': ['unique', 'not_null']},
-      {'name': 'customer_id', 'description': 'Identifier for the entity.', 'data_type': type_string()},
-      {'name': 'anonymous_customer_id', 'description': 'Anonymous identifier for the customer.', 'data_type': type_string(), 'tests': ['not_null']}, 
-      {'name': 'activity', 'description': 'Type of activity performed.', 'data_type': type_string(), 'tests': ['not_null']},
-      {'name': 'ts', 'description': 'Timestamp of when the activity occurred.', 'data_type': type_timestamp(), 'tests': ['not_null']},
-      {'name': 'revenue_impact', 'description': 'Revenue impact of the activity, if applicable.', 'data_type': type_int()},
-      {'name': 'link', 'description': 'Link associated with the activity, if applicable.', 'data_type': type_string()},
-      {'name': 'feature_json', 'description': 'JSON containing additional feature data related to the activity. Contains the following items:', 'data_type': dbt_aql.type_json()},
-      {'name': 'activity_occurrence', 'description': 'Number of times the activity occurred.', 'data_type': type_int()},
-      {'name': 'activity_repeated_at', 'description': 'Timestamp of when the activity was repeated, if applicable.', 'data_type': type_timestamp()}
-  ]
-  %}
-
-    -- Check for anonymous_customer_column
   {% set stream = get_activity_config(activity).stream %}
-  {%- if dbt_aql.anonymous_customer_column(stream) is none -%}
-      {%- set columns = columns | rejectattr("name", "equalto", "anonymous_customer_id") | list -%}
+  {% set schema_columns = dbt_aql.schema_columns() %}
+  {% set customer_column = dbt_aql.customer_column(stream) %}
+  {% set anonymous_customer_column = dbt_aql.anonymous_customer_column(stream) %}
+
+  {% set columns = [
+        {'name': 'activity_id', 'description': 'Unique identifier for the activity.', 'data_type': type_string(), 'tests': ['unique', 'not_null']},
+        {'name': 'customer_id', 'description': 'Identifier for the entity.', 'data_type': type_string()},
+        {'name': 'anonymous_customer_id', 'description': 'Anonymous identifier for the entity.', 'data_type': type_string(), 'tests': ['not_null']}, 
+        {'name': 'activity', 'description': 'Type of activity performed.', 'data_type': type_string(), 'tests': ['not_null']},
+        {'name': 'ts', 'description': 'Timestamp of when the activity occurred.', 'data_type': type_timestamp(), 'tests': ['not_null']},
+        {'name': 'revenue_impact', 'description': 'Revenue impact of the activity, if applicable.', 'data_type': type_int()},
+        {'name': 'link', 'description': 'Link associated with the activity, if applicable.', 'data_type': type_string()},
+        {'name': 'feature_json', 'description': 'JSON containing additional feature data related to the activity. Contains the following items:', 'data_type': dbt_aql.type_json()},
+        {'name': 'activity_occurrence', 'description': 'Number of times the activity occurred.', 'data_type': type_int()},
+        {'name': 'activity_repeated_at', 'description': 'Timestamp of when the activity was repeated, if applicable.', 'data_type': type_timestamp()}
+    ] %}
+
+  -- Remove unused columns
+  {%- if anonymous_customer_column is none -%}
+    {%- set columns = columns | rejectattr("name", "equalto", "anonymous_customer_id") | list -%}
   {%- endif -%}
+
+  {%- if schema_columns.link is not defined -%}
+    {%- set columns = columns | rejectattr("name", "equalto", "link") | list -%}
+  {%- endif -%}
+
+  {%- if schema_columns.revenue_impact is not defined -%}
+    {%- set columns = columns | rejectattr("name", "equalto", "revenue_impact") | list -%}
+  {%- endif -%}
+
+  -- Update column names based on schema_columns
+  {% for column in columns %}
+    {% if column.name in schema_columns %}
+      {% do column.update({'name': schema_columns[column.name]}) %}
+    {% endif %}
+  {% endfor %}
 
   {% do return(columns) %}
 
 {% endmacro %}
+
 
 {% macro generate_activity_yml(activities) %}
 
